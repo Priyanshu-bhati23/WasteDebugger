8310d2d867f35bbf6f02d758ed693b1523c6d792
priyanshu_bhati23 <priyanshubhati23122005@gmail.com>
2025-10-01 10:31:19 +0530

    new updates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Priyanshu Bhati',
+    author_email= 'priyanshubhati.dev@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
--- template.py
+++ template.py
@@ -23,6 +23,7 @@ list_of_files=[
     f"{project_name}/pipeline/training_pipeline.py", 
     f"{project_name}/putils/__init__.py",
     f"{project_name}/utils/main_utils.py",
+    "research/trials.ipynb"
     "templates/index.html",
     "app.py",
     "Dockerfile",
